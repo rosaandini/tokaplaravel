0031214874575f609081076aa9510f47a85580e6
rosaandini <rosaandini0107@gmail.com>
2019-02-27 10:47:44 +0700

    eps11

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -45,7 +45,7 @@ class Kernel extends HttpKernel
     /**
      * The application's route middleware.
      *
-     * These middleware may be assigned to groups or used individually.
+     * These middleware may be assigned to groups or used individually.   
      *
      * @var array
      */
@@ -56,5 +56,6 @@ class Kernel extends HttpKernel
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+        'akses.admin'=>\App\Http\Middleware\AksesAdmin::class,
     ];
 }
